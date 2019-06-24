57f331add5539b9cf6c0d9a348a63a8f3f7a6d3f
jiananarthurli <reudnetwork@gmail.com>
2019-06-24 01:11:07 -0400

    Finished visualization

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,22 +1,43 @@
 function load() {
 
-  var table_of_events = document.getElementById('table_of_events');
+  var event_list = document.getElementById("event_list");
 
-  chrome.storage.local.get('events', function(data) {
+  chrome.storage.local.get("events", function(data) {
 
     if(typeof(data.events) !== "undefined") {
 
-    	var event_table = document.createElement("TABLE");
+      event_list.innerHTML = "";
 
     	data.events.forEach(function(event) {
-  			var row = event_table.insertRow(-1);
+
+        var elmnt = document.createElement("li")
+
   			var event_name = event.name;
-  			var event_link = event_name.link(event.link);
-  			event_link.target = '_blank'; // should open a new tap when clicked
-  			row.innerHTML =  event_link;
-		})
-    	
-    	table_of_events.appendChild(event_table);
+        var event_link = event.link;
+        var event_venue = event.venue;
+
+        var div = document.createElement("div");
+        div.innerHTML = event_venue;
+        div.setAttribute("class", "venue");
+
+        var a = document.createElement("a");
+        // a.innerHTML = '<div class="venue">' + JSON.stringify(event_venue) + '</div>' + event_name;
+        a.innerHTML = event_name;
+        a.setAttribute("target", "_blank");
+        a.setAttribute("href", event_link)
+
+        elmnt.appendChild(div);
+        elmnt.appendChild(a);
+
+        console.log(elmnt);
+
+  			// var a = event_name.link(event_link);
+  			// a.target = '_blank'; // should open a new tap when clicked
+
+        event_list.appendChild(elmnt);
+
+		  })        
+
 
     }
   })
