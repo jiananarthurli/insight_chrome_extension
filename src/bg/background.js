c0e3759ce9a70eb3d6e78c29f92d8c7e12e66897
jiananarthurli <reudnetwork@gmail.com>
2019-07-07 12:00:36 -0400

    Updated the extension name.

--- src/bg/background.js
+++ src/bg/background.js
@@ -15,38 +15,44 @@
 
 function reset (){
 
+	// set the icon to greyscale 
 	chrome.browserAction.setIcon({path : "../../icons/Logo_greyscale.png"});
 
+	// clean the local storage
 	chrome.storage.local.clear(function () {
 		console.log("Events reset");
 	});
 }
 
 reset();
+
+// API server IP
 var api_server = "http://67.205.138.102:8000/";
 
+// Add a listenser when DOM is loaded.
 chrome.webNavigation.onDOMContentLoaded.addListener(function (details) {
 
 	var url = details.url;
 	reset();
 
+	// If en.wikipedia.org is nativaged.
 	if (url.includes("https://en.wikipedia.org/wiki/")) {
 
 		var topic = url.replace("https://en.wikipedia.org/wiki/", "");
 
-		// try to remove the #... anchoring labels but not sure this is working
-		if (topic.includes('#')) {
-			topic = topic.split('#')[0];
-		}
+		// URL for http requests
 		var req_url = api_server + "submission/?wiki_topic=" + topic;
 
+		// Send http requests
 		fetch(req_url)
 		.then(r => r.text())
 		.then(function(result) {
 			result_json = JSON.parse(result);
 			if (result_json.found) {
+				// Store the fetched data into local memory for display
 				chrome.storage.local.set({events: result_json.events}, function() {
 					console.log("Found events");
+					// Change to colored icon
 					chrome.browserAction.setIcon({path : "../../icons/Logo.png"});
         		});
 			}
--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,25 +1,32 @@
 function load() {
 
+  // Get the event_list container
   var event_list = document.getElementById("event_list");
 
+  // Retrieve data from local memory
   chrome.storage.local.get("events", function(data) {
 
+    // If the data is updated
     if(typeof(data.events) !== "undefined") {
 
       event_list.innerHTML = "";
 
+      // Generate list for each event entry
     	data.events.forEach(function(event, idx, array) {
 
+        // Check if the element is the last one. Use a different css style if true.
         if (idx === (array.length - 1)) {
           var elmnt = document.createElement("ul")
         } else {
           var elmnt = document.createElement("li")
         }
 
+        // Obtain the event name, venue and link.
   			var event_name = event.name;
         var event_link = event.link;
         var event_venue = event.venue;
 
+        // Container for the event venue.
         var div = document.createElement("div");
         div.innerHTML = event_venue;
         div.setAttribute("class", "venue");
@@ -27,23 +34,24 @@ function load() {
         var a = document.createElement("a");
 
         a.innerHTML = event_name;
+        // Open a blank tab when the link is clicked.
         a.setAttribute("target", "_blank");
         a.setAttribute("href", event_link);
 
+        // Put the event venue and link to the element
         elmnt.appendChild(div);
         elmnt.appendChild(a);
 
-        console.log(elmnt);
-
+        // Append the new element to the list.
         event_list.appendChild(elmnt);
 
 		  });       
-
 
     }
   })
 }
 
+// Trigger the function when DOM of the pop-up is loaded.
 document.addEventListener('DOMContentLoaded', function() {
 
   load();
